da1ad9ecd1c93de4f70a08b4c8fad7feb6512bb6
Audrey <guilloux.audrey24@gmail.com>
2018-01-29 17:25:41 -0500

    29/01

--- current_chapter.php
+++ current_chapter.php
@@ -51,6 +51,7 @@
 	      <label for="page-number">Page:</label><input type="text" size="3" id="page-number"> sur <span id="number-pages"></span></br>
 	      <button type="button" class="btn btn-outline-secondary" id ="prev"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></button>
         <button type="button" class="btn btn-outline-secondary" id="next"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></button>
+        <p>Tourner les pages avec les flèches du clavier</p>
     </div>
 
     <!-- Footer -->
