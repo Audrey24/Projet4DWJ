330b62214082e8eadd7d045c3e7dbfb2c2e338e4
Audrey24 <31862876+Audrey24@users.noreply.github.com>
2018-06-04 19:42:34 -0400

    Delete database.php

--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'mysql362.sql002:3306');
-define('DB_NAME', 'projetsdsz541');
-define('DB_USER', 'projetsdsz541');
-define('DB_PASS', '24AG92np');
